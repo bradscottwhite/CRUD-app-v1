2b909438860ea46c581d0a666e04a605a9b1a9f6
Brad White <bradscottwhite@gmail.com>
2022-07-15 00:49:39 -0400

    Initialized react frontend shell

---
+++ crud-front-end-v1/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import AppBar from './components/AppBar';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to my CRUD app!</h1>
+      <AppBar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ crud-front-end-v1/src/components/AppBar.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export default () => (
+  <div>
+    <h1>AppBar</h1>
+  </div>
+)
